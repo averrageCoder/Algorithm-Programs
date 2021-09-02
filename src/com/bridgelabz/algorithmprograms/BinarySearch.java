60712e0d96c8781f34afba658dc2d232740be0f4
Harsh R Jain <rjainharsh@gmail.com>
2021-09-03 01:04:27 +0530

    REFACTORED: to enable binary search for generic datatypes

--- src/com/bridgelabz/algorithmprograms/BinarySearch.java
+++ src/com/bridgelabz/algorithmprograms/BinarySearch.java
@@ -1,47 +1,49 @@
 package com.bridgelabz.algorithmprograms;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //binary search of given words
 public class BinarySearch {
-	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String[] words = {"today","is","a","wonderful","day"};
-		int n = words.length;
-
-		String temp;
-		System.out.println("Strings in sorted order:");
-		for (int j = 0; j < words.length; j++) {
-	   	   for (int i = j + 1; i < words.length; i++) {
-			if (words[i].compareTo(words[j]) < 0) {
-				temp = words[j];
-				words[j] = words[i];
-				words[i] = temp;
-			}
-		   }
-		}
-		for (int m = 0; m < words.length; m++) {
-			System.out.print(words[m] + " ");
-		}
-
-		System.out.println("\nEnter the word you want to search for: ");
-		String word = scn.next();
+	
+	public static <E extends Comparable<E>> void binarySearch(E[] inputArray, E key) {
+		int n = inputArray.length;
 		int li = 0, hi = n - 1, mi = 0;
 		boolean flag=false;
 
 		while (li <= hi) {
 			mi = (li + hi) / 2;
-			if (words[mi].equals(word)) {
+			if (inputArray[mi].equals(key)) {
 				flag=true;
 				break;
-			} else if ((words[mi].compareTo(word)) < 0) {
+			} else if ((inputArray[mi].compareTo(key)) < 0) {
 				li = mi + 1;
 			} else {
 				hi = mi-1;
 			}
 		}
-		if (flag) System.out.println("\""+word + "\" is word no. " + (mi + 1));
+		if (flag) System.out.println("\""+key + "\" is at position no. " + (mi + 1));
 		else System.out.println("Not found!");
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		//String[] inputArray = {"today","is","a","wonderful","day"};
+		Integer inputArray[] = { 1,5,-1,45,-10};
+		System.out.println("Input Array in sorted order:");
+		Arrays.sort(inputArray);
+		
+		for (int m = 0; m < inputArray.length; m++) {
+			System.out.print(inputArray[m] + " ");
+		}
+
+		System.out.println("\nEnter the key you want to search for: ");
+		//String key = scn.next();
+		Integer key = scn.nextInt();
+		
+		binarySearch(inputArray, key);
+		
 	}
 }
