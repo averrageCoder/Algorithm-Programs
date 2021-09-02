941b0df0cd522b6bd9e50d9830c1426b51cf9e94
Harsh R Jain <rjainharsh@gmail.com>
2021-09-02 23:30:25 +0530

    ADDED: program to sort a string array using merge sort

---
+++ src/com/bridgelabz/algorithmprograms/MergeSort.java
@@ -0,0 +1,71 @@
+package com.bridgelabz.algorithmprograms;
+
+import java.util.Arrays;
+
+class MergeSort
+{
+
+    void merge(String arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        String L[] = new String[n1];
+        String R[] = new String[n2];
+ 
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+ 
+        int i = 0, j = 0;
+ 
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+    void sort(String arr[], int l, int r)
+    {
+        if (l < r) {
+            int m =l+ (r-l)/2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+  
+    public static void main(String args[])
+    {
+    	String[] inputArray = {"Emma","Dave","Cindy","Bom","Andy","Bob"};
+ 
+        System.out.println("Before merge sort: "+ Arrays.toString(inputArray));
+ 
+        MergeSort ob = new MergeSort();
+        ob.sort(inputArray, 0, inputArray.length - 1);
+ 
+        System.out.println("After merge sort: "+ Arrays.toString(inputArray));
+    }
+}
